bcae087930097c8e8ab1d271f1a7209fd3717894
Bernard-Calma <nard.calma@gmail.com>
2022-11-30 21:47:48 -0800

    text press to console colors

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+
 import { Main } from './Containers/Main';
 
 const App = () => {
--- src/Containers/Main.js
+++ src/Containers/Main.js
@@ -1,24 +1,50 @@
 // Import Styling
 
+// Color List
+const colorList = [
+    "red",
+    "blue",
+    "green"
+]
 
 // Main Page
 export const Main = () => {
-    return (
-        // Title
-        <h1>Papuros</h1>
-        
-        // Background
-        // Notes List
-        // Add Notes Container
-        // Delete Notes Container
-        // Minimize Notes Container
-        // Hide Notes Container
-        // Notes Container
-        // Change Font Color
-        // Change Notes Container Size
-        // Change background color
 
-        // About
-        // Help
+    const changeBackgroundColor = (color) => {
+        console.log(color)
+    }
+
+    return (
+        <>
+            {/* // Title */}
+            <h1>Papuros</h1>
+            <div style={{display: "flex", flexDirection: "column"}}>
+            {
+                colorList.map(color => 
+                <p 
+                    key = {color}
+                    onClick={() => changeBackgroundColor(color)}
+                    >
+                    {color}
+                </p>)
+            }
+            </div>
+           
+            
+            {/* // Background
+            // Notes List
+            // Add Notes Container
+            // Delete Notes Container
+            // Minimize Notes Container
+            // Hide Notes Container
+            // Notes Container
+            // Change Font Color
+            // Change Notes Container Size
+            // Change Notes Container Color
+            // Change background color
+
+            // About
+            // Help */}
+        </>
     )
 }
